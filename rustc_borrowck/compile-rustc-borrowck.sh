4e0be7d2182376e0f059215d9907208edaae2679
Jasper Geer <jasper.geer@gmail.com>
2025-06-26 22:35:30 -0700

    aaaaaaaaa

--- rustc_borrowck/compile-rustc-borrowck.sh
+++ rustc_borrowck/compile-rustc-borrowck.sh
@@ -10,5 +10,9 @@ case "$arch" in
     SMALLVEC_PATH="/usr/local/rustup/toolchains/nightly-2024-12-15-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-linux-gnu/lib/libsmallvec-37653000af668d0c.rmeta"
     RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
     ;;
+  *)
+    echo "Unsupported architecture: $arch"
+    exit 1
+    ;;
 esac
 
