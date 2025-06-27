2ff1eb212340be7e33fb22d7d1a67b3395b5c01b
Jasper Geer <jasper.geer@gmail.com>
2025-06-26 21:33:22 -0700

    ok maybe the second

--- rustc_borrowck/compile-rustc-borrowck.sh
+++ rustc_borrowck/compile-rustc-borrowck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SMALLVEC_PATH=$(find "$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | awk '/host:/ {print $2}')/lib" -type f -name 'libsmallvec-*.rmeta' | head -n 1)
+SMALLVEC_PATH=$(find "$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | awk '/host:/ {print $2}')/lib" -type f -name 'libsmallvec-*.rmeta' | sed -n 2p)
 echo $SMALLVEC_PATH
 RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
