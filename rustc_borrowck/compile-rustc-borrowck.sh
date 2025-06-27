14018942793a2b7a4f30c6e6127ef28f95f88a1a
Jasper Geer <jasper.geer@gmail.com>
2025-06-26 22:02:09 -0700

    try specifying some env

--- rustc_borrowck/compile-rustc-borrowck.sh
+++ rustc_borrowck/compile-rustc-borrowck.sh
@@ -2,4 +2,4 @@
 
 SMALLVEC_PATH="/usr/local/rustup/toolchains/nightly-2024-12-15-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-linux-gnu/lib/libsmallvec-37653000af668d0c.rmeta"
 echo $SMALLVEC_PATH
-RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
+CARGO_HOME=/usr/local/cargo RUSTUP_HOME=/usr/local/rustup RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
