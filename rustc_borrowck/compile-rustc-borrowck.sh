afd229800e13a63185306827bddc6d1968d82328
Jasper Geer <jasper.geer@gmail.com>
2025-06-26 21:36:21 -0700

    break this on purpose

--- rustc_borrowck/compile-rustc-borrowck.sh
+++ rustc_borrowck/compile-rustc-borrowck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SMALLVEC_PATH=$(find "$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | awk '/host:/ {print $2}')/lib" -type f -name 'libsmallvec-*.rmeta' | sed -n 2p)
+SMALLVEC_PATH=$(find "$(rustc --print sysroot)/lib/rustlib/$(rustc -vV | awk '/host:/ {print $2}')/lib" -type f -name 'libsmallvec-*.rmeta')
 echo $SMALLVEC_PATH
 RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
