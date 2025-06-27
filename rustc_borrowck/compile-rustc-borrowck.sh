4925581fb37670db7ba9eeb4468f6e000a2e32a9
Jasper Geer <jasper.geer@gmail.com>
2025-06-26 22:33:47 -0700

    commiting crimes

--- rustc_borrowck/compile-rustc-borrowck.sh
+++ rustc_borrowck/compile-rustc-borrowck.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-SMALLVEC_PATH="/usr/local/rustup/toolchains/nightly-2024-12-15-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-linux-gnu/lib/libsmallvec-37653000af668d0c.rmeta"
 echo $SMALLVEC_PATH
-CARGO_HOME=/usr/local/cargo RUSTUP_HOME=/usr/local/rustup RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
+
+case "$arch" in
+  x86_64)
+    cargo build
+    ;;
+  aarch64)
+    SMALLVEC_PATH="/usr/local/rustup/toolchains/nightly-2024-12-15-aarch64-unknown-linux-gnu/lib/rustlib/aarch64-unknown-linux-gnu/lib/libsmallvec-37653000af668d0c.rmeta"
+    RUSTFLAGS="--extern smallvec=$SMALLVEC_PATH" cargo build
+    ;;
+esac
+
